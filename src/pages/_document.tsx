0646136e66de401b53f50023bba86e2705c0da6a
segersniels <segers.n@hotmail.com>
2023-03-26 13:35:48 +0200

    :sparkles: Add generated favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,11 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+      <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👋</text></svg>"
+        />
+
         <meta
           name="description"
           content="Generate your gitmoji commit message"
