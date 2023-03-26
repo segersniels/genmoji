c4a8bac1b4e899557549f2c31679ed061fa28193
segersniels <segers.n@hotmail.com>
2023-03-26 13:21:23 +0200

    :heavy_plus_sign: Add @vercel/analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import 'styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Inter } from 'next/font/google';
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -11,6 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={`${inter.variable} font-sans`}>
       <Component {...pageProps} />
+      <Analytics />
     </main>
   );
 }
