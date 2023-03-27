30f957c46c448fd9ae1b9d2ad7d7b8248e2016de
segersniels <segers.n@hotmail.com>
2023-03-27 20:10:58 +0200

    :sparkles: Add NPM link to Footer component

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,10 +1,17 @@
 import Link from 'next/link';
 import styles from './Footer.module.css';
-import { FaGithub } from 'react-icons/fa';
+import { FaGithub, FaNpm } from 'react-icons/fa';
 
 const Footer = () => {
   return (
     <div className={styles.container}>
+      <Link
+        href="https://www.npmjs.com/package/@segersniels/gitmoji"
+        target="_blank"
+      >
+        <FaNpm className={styles.icon} />
+      </Link>
+
       <Link href="https://github.com/segersniels/genmoji" target="_blank">
         <FaGithub className={styles.icon} />
       </Link>
