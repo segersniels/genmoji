003d62d8031dc9a530b53fc108e2a46388d45f08
segersniels <segers.n@hotmail.com>
2023-03-29 13:47:09 +0200

    :ambulance: Fix prompt for better messages

--- src/pages/api/generate.ts
+++ src/pages/api/generate.ts
@@ -61,14 +61,13 @@ export default async function handler(
 ) {
   const data = await fetchGitmojis();
   const prompt = `
-    Refer to the provided git diff or code snippet and provide a suitable gitmoji commit message.
-
-    Here is a list of gitmoji codes and their descriptions:
-    ${data.choices}
+    Refer to the provided git diff or code snippet and provide a suitable commit message.
 
     When reviewing the diff or code, focus on identifying the main purpose of the changes.
     Are they fixing a bug, adding a new feature, improving performance or readability, or something else?
-    Use this information to craft a concise and meaningful gitmoji commit message that clearly indicates what the provided snippet does.
+    Use this information to craft a concise and detailed gitmoji commit message that clearly describes what the provided code or diff does.
+
+    Describe the change to the best of your capabilities in one short sentence. Don't go into too much detail.
 
     When reviewing a diff, pay attention to the changed filenames and extract the context of the changes.
     This will help you create a more relevant and informative commit message.
@@ -77,21 +76,28 @@ export default async function handler(
       - Markdown files are usually related to documentation.
       - Config file adjustments are usually related to configuration changes.
 
-    Don't do this:
-      - :bug: Fix issue with shopping cart checkout process
-      - :zap: Improve performance of search functionality
-      - :lipstick: Refactor styling for product details page
-      - :memo: Update documentation for API endpoints
+    Here is a list of gitmoji codes and their descriptions of what they mean when they are used: """
+    ${data.choices}
+    """
 
-    Do this:
+    Try to match the generated message to a fitting emoji using its description from the provided list above.
+    So go look in the descriptions and find the one that best matches the description.
+    So a change related to a refactor would best match with ":recycle: - Refactor code." from the list and would choose :recycle: as the gitmoji.
+
+    Always start your commit message with a gitmoji followed by the message starting with a capital letter.
+    Never mention filenames or function names in the message.
+
+    Don't do this:
       - :bug: Fix issue in calculateTotalPrice function
       - :zap: Improve performance of calculateTopProducts function
       - :lipstick: Refactor styling for calculateCartTotal function
       - :memo: Update documentation for getProductById function
 
-    Limit yourself to one sentence but don't end it in a punctuation mark.
-    Always start your commit message with a gitmoji followed by the message starting with a capital letter.
-    Never mention file names or function names in the message.
+    Do this:
+      - :bug: Fix issue with shopping cart checkout process
+      - :zap: Improve performance of search functionality
+      - :lipstick: Refactor styling for product details page
+      - :memo: Update documentation for API endpoints
 
     ${
       req.body.context
