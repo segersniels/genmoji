8412b69546ae707a01788659221abe6c4a13abfe
segersniels <segers.n@hotmail.com>
2023-03-30 10:35:27 +0200

    :bug: Fix lockfile changes appearing in diffs

--- src/pages/api/generate.ts
+++ src/pages/api/generate.ts
@@ -38,6 +38,39 @@ async function fetchGitmojis() {
   };
 }
 
+const FILES_TO_IGNORE = [
+  'package-lock.json',
+  'yarn.lock',
+  'npm-debug.log',
+  'yarn-debug.log',
+  'yarn-error.log',
+  '.pnpm-debug.log',
+];
+
+/**
+ * Attempt to remove lockfile changes from the diff
+ */
+export function removeLockfileChanges(diff: string) {
+  const result = Array.from(
+    diff.matchAll(/diff --git[\s\S]*?(?=diff --git|$)/g),
+    (match) => match[0]
+  );
+
+  return result
+    .filter((chunk) => {
+      const firstLine = chunk.split('\n')[0];
+
+      for (const file of FILES_TO_IGNORE) {
+        if (firstLine.includes(file)) {
+          return false;
+        }
+      }
+
+      return true;
+    })
+    .join('\n');
+}
+
 /**
  * Do some additional post processing on the received answer
  */
@@ -110,7 +143,7 @@ export default async function handler(
     }
 
     Here is the provided git diff or code snippet: """
-    ${req.body.code}
+    ${removeLockfileChanges(req.body.code)}
     """
   `;
 
