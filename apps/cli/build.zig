8c4290f3d8a5257736224e8fdea486523bee3bad
segersniels <segers.n@hotmail.com>
2024-03-29 11:56:08 +0100

    :technologist: Improve turbo setup

--- apps/cli/build.zig
+++ apps/cli/build.zig
@@ -58,7 +58,7 @@ pub fn build(b: *std.Build) !void {
     const optimize = b.standardOptimizeOption(.{});
 
     const exe = b.addExecutable(.{
-        .name = "cli",
+        .name = "genmoji",
         // In this case the main source file is merely a path, however, in more
         // complicated build scripts, this could be a generated file.
         .root_source_file = .{ .path = "src/main.zig" },
