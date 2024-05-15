d27bcc0db76fcd57883e10134a1228a847acd9a5
segersniels <segers.n@hotmail.com>
2024-05-15 11:20:01 +0200

    :recycle: Just use regular 3.5 turbo

--- apps/web/src/app/api/chat/route.ts
+++ apps/web/src/app/api/chat/route.ts
@@ -29,7 +29,7 @@ export async function POST(req: Request) {
 
   const response = await openai.createChatCompletion({
     user: 'genmoji.dev',
-    model: 'gpt-3.5-turbo-16k',
+    model: 'gpt-3.5-turbo',
     messages: [
       {
         role: 'system',
