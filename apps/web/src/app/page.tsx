9e68f47b47ae37a381ec2d7187b7a71d71c244b9
segersniels <segers.n@hotmail.com>
2024-05-29 15:43:46 +0200

    :recycle: Redesign home screen

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -3,46 +3,35 @@ import Link from 'next/link';
 import Image from 'next/image';
 import Demo from '../../../cli/demo.gif';
 import { FaArrowRight, FaGithub } from 'react-icons/fa';
+import { Button } from 'components/ui/button';
 
-export const runtime = 'edge';
-
-export default async function Page() {
+export default function Page() {
   return (
-    <div className="space-y-8">
-      <div className="flex flex-col overflow-hidden sm:flex-row sm:rounded-md sm:border">
-        <div className="hidden flex-1 sm:block">
-          <Image src={Demo} alt="demo" />
-        </div>
+    <div className="flex flex-col items-center space-y-12 p-4 text-center">
+      <div className="space-y-4">
+        <h1 className="text-5xl font-bold tracking-tight sm:text-6xl">
+          Generate gitmoji commit messages
+        </h1>
 
-        <div className="flex w-96 flex-col justify-center space-y-4 px-4 text-center sm:p-12">
-          <h1 className="text-5xl font-bold tracking-tighter sm:text-5xl">
-            Generate your{' '}
-            <Link href="https://gitmoji.dev" target="_blank">
-              gitm<p className="inline text-4xl sm:text-4xl">😀</p>ji
-            </Link>{' '}
-            commit message.
-          </h1>
-
-          <div className="flex flex-col items-center space-y-4">
-            <p className="leading-tight text-muted-foreground">
-              Grab the CLI tool from GitHub below and generate your commit
-              message from the comfort of your terminal.
-            </p>
-
-            <Link href="https://github.com/segersniels/genmoji" target="_blank">
-              <FaGithub className="h-8 w-8 hover:text-gray-700" />
-            </Link>
-          </div>
+        <div className="flex flex-col items-center space-y-4">
+          <p className="text-center text-xl leading-tight text-muted-foreground sm:w-[512px]">
+            Why spend time thinking about commit messages when you can generate
+            them using AI?
+          </p>
         </div>
       </div>
 
-      <div className="flex w-full flex-col items-center space-y-8 px-4 sm:px-0">
-        <Link href="/web">
-          <h2 className="text-xl font-bold tracking-tighter sm:text-3xl">
-            Or try out the web version here{' '}
-            <FaArrowRight className="inline h-4 w-4" />
-          </h2>
-        </Link>
+      <div className="flex flex-col justify-center space-x-4">
+        <Button className="bg-[#ffdd67] text-black">Download</Button>
+        <Button variant="link" asChild>
+          <Link href="/web" className="text-xs">
+            Or try it out here <FaArrowRight className="ml-1 inline h-2 w-2" />
+          </Link>
+        </Button>
+      </div>
+
+      <div className="hidden flex-1 overflow-hidden rounded-2xl shadow-lg sm:block">
+        <Image src={Demo} alt="demo" />
       </div>
     </div>
   );
--- apps/web/src/app/web/page.tsx
+++ apps/web/src/app/web/page.tsx
@@ -1,21 +1,12 @@
 import { getGitmojis } from 'app/actions';
-import Form from 'components/form';
-import Link from 'next/link';
+import Client from './client';
 
 export default async function Web() {
   const gitmojis = await getGitmojis();
 
   return (
-    <div className="flex w-full flex-col items-center justify-center space-y-8 px-4 sm:w-[25vw] sm:px-0">
-      <h1 className="break-words text-center text-6xl font-bold tracking-tighter">
-        Generate your{' '}
-        <Link href="https://gitmoji.dev" target="_blank">
-          gitm<p className="inline text-4xl sm:text-5xl">😀</p>ji
-        </Link>{' '}
-        commit message.
-      </h1>
-
-      <Form className="w-full" gitmojis={gitmojis} />
+    <div className="flex w-full flex-col items-center justify-center space-y-4 px-4 sm:w-[25vw] sm:px-0">
+      <Client className="w-full" gitmojis={gitmojis} />
     </div>
   );
 }
