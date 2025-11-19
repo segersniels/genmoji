8f20e7b1c28c4220923cc6b9038aed4baa8ba4fc
segersniels <github@niels.foo>
2025-11-19 10:14:38 +0100

    :sparkles: enable adsense

--- apps/web/app/root.tsx
+++ apps/web/app/root.tsx
@@ -44,6 +44,12 @@ export function Layout({ children }: { children: React.ReactNode }) {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap"
           rel="stylesheet"
         />
+
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9693962508925240"
+          crossOrigin="anonymous"
+        />
       </head>
 
       <body className="h-screen min-h-screen bg-background font-sans antialiased">
